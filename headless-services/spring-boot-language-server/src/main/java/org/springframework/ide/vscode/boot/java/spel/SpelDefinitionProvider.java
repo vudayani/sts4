bc83d759bf61fa7d3bc57bc118dc7623c92c0ea5
vudayani <vudayani@vmware.com>
2024-08-27 18:48:07 +0530

    filter based on offset, add tests

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/spel/SpelDefinitionProvider.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/spel/SpelDefinitionProvider.java
@@ -31,35 +31,34 @@ import org.springframework.ide.vscode.commons.protocol.spring.Bean;
 import org.springframework.ide.vscode.parser.spel.SpelLexer;
 import org.springframework.ide.vscode.parser.spel.SpelParser;
 import org.springframework.ide.vscode.parser.spel.SpelParser.BeanReferenceContext;
-import org.springframework.ide.vscode.parser.spel.SpelParser.PossiblyQualifiedIdContext;
-import org.springframework.ide.vscode.parser.spel.SpelParser.TypeReferenceContext;
 import org.springframework.ide.vscode.parser.spel.SpelParserBaseListener;
 
 public class SpelDefinitionProvider implements IJavaDefinitionProvider {
-	
+
 	protected static Logger logger = LoggerFactory.getLogger(SpelDefinitionProvider.class);
-	
+
 	private final SpringMetamodelIndex springIndex;
-	
+
 	private final AnnotationParamSpelExtractor[] spelExtractors = AnnotationParamSpelExtractor.SPEL_EXTRACTORS;
-	
+
 	public SpelDefinitionProvider(SpringMetamodelIndex springIndex) {
 		this.springIndex = springIndex;
 	}
 
 	@Override
-	public List<LocationLink> getDefinitions(CancelChecker cancelToken, IJavaProject project, CompilationUnit cu, ASTNode n, int offset) {
-		logger.info("offset", offset);
+	public List<LocationLink> getDefinitions(CancelChecker cancelToken, IJavaProject project, CompilationUnit cu,
+			ASTNode n, int offset) {
 		if (n instanceof StringLiteral) {
 			StringLiteral valueNode = (StringLiteral) n;
 			logger.info("value node " + valueNode);
-			
+
 			ASTNode parent = ASTUtils.getNearestAnnotationParent(valueNode);
-			
+
 			if (parent != null && parent instanceof Annotation) {
 				Annotation a = (Annotation) parent;
 				IAnnotationBinding binding = a.resolveAnnotationBinding();
-				if (binding != null && binding.getAnnotationType() != null && Annotations.VALUE.equals(binding.getAnnotationType().getQualifiedName())) {
+				if (binding != null && binding.getAnnotationType() != null
+						&& Annotations.VALUE.equals(binding.getAnnotationType().getQualifiedName())) {
 					return parseSpelAndFetchLocation(cancelToken, project, cu, offset);
 				}
 			}
@@ -67,16 +66,18 @@ public class SpelDefinitionProvider implements IJavaDefinitionProvider {
 		return Collections.emptyList();
 	}
 
-	private List<LocationLink> parseSpelAndFetchLocation(CancelChecker cancelToken, IJavaProject project, CompilationUnit cu, int offset) {
+	private List<LocationLink> parseSpelAndFetchLocation(CancelChecker cancelToken, IJavaProject project,
+			CompilationUnit cu, int offset) {
 		List<LocationLink> locationLink = new ArrayList<>();
 		cu.accept(new ASTVisitor() {
 			@Override
 			public boolean visit(SingleMemberAnnotation node) {
+				logger.info("offset : " + offset);
 				Arrays.stream(spelExtractors).map(e -> e.getSpelRegion(node)).filter(o -> o.isPresent())
-				.map(o -> o.get()).forEach(snippet -> {
-							List<String> tokens = computeTokens(snippet.text(), offset);
+						.map(o -> o.get()).forEach(snippet -> {
+							List<Token> tokens = computeTokens(snippet.text(), offset);
 							if (tokens != null && tokens.size() > 0) {
-								locationLink.addAll(findBeansWithName(project, tokens.get(0)));
+								locationLink.addAll(findLocationLinksForOffsetTokens(project, offset, tokens));
 							}
 						});
 				return super.visit(node);
@@ -84,13 +85,12 @@ public class SpelDefinitionProvider implements IJavaDefinitionProvider {
 
 			@Override
 			public boolean visit(NormalAnnotation node) {
-				
 
 				Arrays.stream(spelExtractors).map(e -> e.getSpelRegion(node)).filter(o -> o.isPresent())
-				.map(o -> o.get()).forEach(snippet -> {
-							List<String> tokens = computeTokens(snippet.text(), 0);
+						.map(o -> o.get()).forEach(snippet -> {
+							List<Token> tokens = computeTokens(snippet.text(), 0);
 							if (tokens != null && tokens.size() > 0) {
-								locationLink.addAll(findBeansWithName(project, tokens.get(0)));
+								locationLink.addAll(findLocationLinksForOffsetTokens(project, offset, tokens));
 							}
 						});
 
@@ -102,54 +102,51 @@ public class SpelDefinitionProvider implements IJavaDefinitionProvider {
 
 	private List<LocationLink> findBeansWithName(IJavaProject project, String beanName) {
 		Bean[] beans = this.springIndex.getBeansWithName(project.getElementName(), beanName);
-		
-		return Arrays.stream(beans)
-				.map(bean -> {
-					return new LocationLink(bean.getLocation().getUri(), bean.getLocation().getRange(), bean.getLocation().getRange());
-				})
-				.collect(Collectors.toList());
+
+		return Arrays.stream(beans).map(bean -> {
+			return new LocationLink(bean.getLocation().getUri(), bean.getLocation().getRange(),
+					bean.getLocation().getRange());
+		}).collect(Collectors.toList());
 	}
-	
-	private List<String> computeTokens(String text, int initialOffset) {
+
+	private List<LocationLink> findLocationLinksForOffsetTokens(IJavaProject project, int offset, List<Token> tokens) {
+		return tokens.stream().filter(t -> isOffsetWithinToken(t, offset))
+				.flatMap(t -> findBeansWithName(project, t.getText()).stream()).collect(Collectors.toList());
+	}
+
+	private boolean isOffsetWithinToken(Token token, int offset) {
+		int startIndex = token.getStartIndex();
+		int endIndex = startIndex + token.getText().length();
+		return startIndex <= (offset - 10) && (offset - 10) <= endIndex;
+	}
+
+	private List<Token> computeTokens(String text, int offset) {
 		SpelLexer lexer = new SpelLexer(CharStreams.fromString(text));
 		CommonTokenStream antlrTokens = new CommonTokenStream(lexer);
 		SpelParser parser = new SpelParser(antlrTokens);
-		
+
 		lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
 		parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
-		
-		List<String> tokens = new ArrayList<>();
-		
+
+		List<Token> tokens = new ArrayList<>();
+
 		parser.addParseListener(new SpelParserBaseListener() {
-			
 
 			@Override
 			public void exitBeanReference(BeanReferenceContext ctx) {
 				if (ctx.IDENTIFIER() != null) {
-					Token sym = ctx.IDENTIFIER().getSymbol();
-//					logger.info(text.substring(sym.getStartIndex() + initialOffset, sym.getStartIndex()+sym.getText().length()+initialOffset));
-					
-					tokens.add(ctx.IDENTIFIER().getSymbol().getText());
+					tokens.add(ctx.IDENTIFIER().getSymbol());
 				}
 				if (ctx.STRING_LITERAL() != null) {
-					tokens.add(ctx.STRING_LITERAL().getSymbol().getText());
+					tokens.add(ctx.STRING_LITERAL().getSymbol());
 				}
 			}
-			
-			@Override
-			public void exitTypeReference(TypeReferenceContext ctx) {
-				PossiblyQualifiedIdContext parserContext = ctx.possiblyQualifiedId();
-				parserContext.getText();
-				super.exitTypeReference(ctx);
-			}
 
-		});		
-		
+		});
+
 		parser.spelExpr();
 		return tokens;
 
 	}
-
-
 
 }
---
+++ headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/java/spel/SpelDefinitionProviderTest.java
@@ -0,0 +1,153 @@
+package org.springframework.ide.vscode.boot.java.spel;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.LocationLink;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.ide.vscode.boot.app.SpringSymbolIndex;
+import org.springframework.ide.vscode.boot.bootiful.BootLanguageServerTest;
+import org.springframework.ide.vscode.boot.bootiful.SymbolProviderTestConf;
+import org.springframework.ide.vscode.boot.index.SpringMetamodelIndex;
+import org.springframework.ide.vscode.commons.java.IJavaProject;
+import org.springframework.ide.vscode.commons.languageserver.java.JavaProjectFinder;
+import org.springframework.ide.vscode.commons.protocol.spring.Bean;
+import org.springframework.ide.vscode.commons.util.text.LanguageId;
+import org.springframework.ide.vscode.languageserver.testharness.Editor;
+import org.springframework.ide.vscode.project.harness.BootLanguageServerHarness;
+import org.springframework.ide.vscode.project.harness.ProjectsHarness;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@BootLanguageServerTest
+@Import(SymbolProviderTestConf.class)
+public class SpelDefinitionProviderTest {
+
+	@Autowired
+	private BootLanguageServerHarness harness;
+	@Autowired
+	private JavaProjectFinder projectFinder;
+	@Autowired
+	private SpringMetamodelIndex springIndex;
+	@Autowired
+	private SpringSymbolIndex indexer;
+
+	private File directory;
+	private IJavaProject project;
+
+	private Bean visitService;
+	private Bean spelExpressionsClass;
+	private String expectedDefinitionUriVisitService;
+	private String expectedDefinitionUriSpelClass;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		harness.intialize(null);
+
+		directory = new File(ProjectsHarness.class.getResource("/test-projects/test-spring-indexing/").toURI());
+
+		String projectDir = directory.toURI().toString();
+		project = projectFinder.find(new TextDocumentIdentifier(projectDir)).get();
+
+		expectedDefinitionUriVisitService = directory.toPath().resolve("src/main/java/org/test/VisitService.java").toUri().toString();
+		expectedDefinitionUriSpelClass = directory.toPath().resolve("src/main/java/org/test/SpelExpressionsClass.java").toUri().toString();
+		visitService = new Bean("visitService", "org.test.VisitService",
+				new Location(expectedDefinitionUriVisitService, new Range(new Position(4, 0), new Position(4, 8))),
+				null, null, null);
+		spelExpressionsClass = new Bean("spelExpressionsClass", "org.test.SpelExpressionsClass",
+				new Location(expectedDefinitionUriSpelClass, new Range(new Position(7, 0), new Position(7, 11))), null,
+				null, null);
+
+		springIndex.updateBeans(project.getElementName(), new Bean[] { visitService, spelExpressionsClass });
+
+		CompletableFuture<Void> initProject = indexer.waitOperation();
+		initProject.get(5, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void testBeanDefinitionLinkInSpel() throws Exception {
+		String tempJavaDocUri = directory.toPath().resolve("src/main/java/org/test/TempClass.java").toUri().toString();
+
+		Editor editor = harness.newEditor(LanguageId.JAVA, """
+				package org.test;
+
+				import org.springframework.beans.factory.annotation.Value;
+				import org.springframework.stereotype.Controller;
+				import org.springframework.web.bind.annotation.GetMapping;
+				import org.springframework.web.bind.annotation.ResponseBody;
+
+				@Controller
+				public class SpelExpressionsClass {
+
+					@Value("${app.version}")
+					private String appVersion;
+
+					@Value("#{@visitService.isValidVersion('${app.version}') ? 'Valid Version' :'Invalid Version'}")
+					private String versionValidity;
+				}""", tempJavaDocUri);
+
+		Bean[] beans = springIndex.getBeansWithName(project.getElementName(), "visitService");
+		assertEquals(1, beans.length);
+
+		LocationLink expectedLocation = new LocationLink(expectedDefinitionUriVisitService,
+				beans[0].getLocation().getRange(), beans[0].getLocation().getRange(), null);
+
+		editor.assertLinkTargets("visitService", List.of(expectedLocation));
+	}
+
+	@Test
+	public void testMultipleBeanDefinitionLinksInSpel() throws Exception {
+
+		String tempJavaDocUri = directory.toPath().resolve("src/main/java/org/test/TempClass.java").toUri().toString();
+
+		Editor editor = harness.newEditor(LanguageId.JAVA,
+				"""
+						package org.test;
+
+						import org.springframework.beans.factory.annotation.Value;
+						import org.springframework.stereotype.Controller;
+						import org.springframework.web.bind.annotation.GetMapping;
+						import org.springframework.web.bind.annotation.ResponseBody;
+
+						@Controller
+						public class SpelExpressionsClass {
+
+							@Value("${app.version}")
+							private String appVersion;
+
+							@Value("#{@visitService.isValidVersion('${app.version}') ? @spelExpressionsClass.toUpperCase('valid') :@spelExpressionsClass.text2('invalid version)}")
+							private String fetchVersion;
+						}""",
+				tempJavaDocUri);
+
+		Bean[] visitServiceBean = springIndex.getBeansWithName(project.getElementName(), "visitService");
+		assertEquals(1, visitServiceBean.length);
+
+		Bean[] spelExpBean = springIndex.getBeansWithName(project.getElementName(), "spelExpressionsClass");
+		assertEquals(1, spelExpBean.length);
+
+		LocationLink expectedLocation1 = new LocationLink(expectedDefinitionUriVisitService,
+				visitServiceBean[0].getLocation().getRange(), visitServiceBean[0].getLocation().getRange(), null);
+
+		LocationLink expectedLocation2 = new LocationLink(expectedDefinitionUriSpelClass,
+				spelExpBean[0].getLocation().getRange(), spelExpBean[0].getLocation().getRange(), null);
+
+		editor.assertLinkTargets("visitService", List.of(expectedLocation1));
+		editor.assertLinkTargets("spelExpressionsClass", List.of(expectedLocation2));
+
+	}
+
+}
